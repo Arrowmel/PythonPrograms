806126069a61ef2879530e99838654428314cd95
Arrowmel <110785171+Arrowmel@users.noreply.github.com>
2022-08-08 00:53:19 +0530

    Create Positive.py
    This program is used for printing the positive number in a list

---
+++ Positive.py
@@ -0,0 +1,10 @@
+list1=[12,-7,5,64,-14]
+print("Output of list1 is:")
+for i in list1:
+    if i>0:
+        print(i)
+list2=[12,14,-95,3]
+print("Output of list2 is:")
+for j in list2:
+    if j>0:
+        print(j)
